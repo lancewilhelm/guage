cdef3b89b90f8aaadc72018593a124fd93e8e9f5
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-03-29 00:45:03 -0400

    feat: db stats in settings

---
+++ app/api/stats/route.ts
@@ -0,0 +1,36 @@
+import { logger } from "@/utils/logger";
+import { NextResponse } from "next/server";
+import { messagesTable, chatsTable } from "@/utils/db/schema";
+import { cloudDb } from "@/utils/db/cloud";
+import { count } from "drizzle-orm";
+import { getSession } from "@/utils/auth";
+
+export async function GET() {
+  logger.debug("GET /api/stats");
+
+  // Ensure the user is authenticated
+  const session = await getSession();
+  if (!session || !["admin", "owner"].includes(session.user.role)) {
+    logger.warn("GET /api/sync: Unauthorized access attempt");
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  // const userId = session.user.id;
+
+  try {
+    const chatCount = await cloudDb.select({ count: count() }).from(chatsTable);
+
+    const messageCount = await cloudDb
+      .select({ count: count() })
+      .from(messagesTable);
+
+    return NextResponse.json({
+      chatCount: chatCount[0].count,
+      messageCount: messageCount[0].count,
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    logger.error("Error in nuke:", error);
+    return NextResponse.json({ error: "Failed to nuke" }, { status: 500 });
+  }
+}
