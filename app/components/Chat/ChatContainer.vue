e4ca762e905204d818050e2a2cc3eb1486d895fb
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-04-17 11:05:41 -0400

    fix: removing focus outline for chat container

--- app/components/Chat/ChatContainer.vue
+++ app/components/Chat/ChatContainer.vue
@@ -74,7 +74,7 @@ watch(inputPadding, () => {
 <template>
   <div
     ref="containerRef"
-    class="h-full w-full overflow-x-hidden overflow-y-auto pt-2 chat-container"
+    class="h-full w-full overflow-x-hidden overflow-y-auto pt-2 chat-container focus:outline-none"
   >
     <div class="max-w-(--chat-max-width) mx-auto px-6 flex flex-col gap-4">
       <div v-for="message in activeMessages" :key="message?.id" v-measure>
