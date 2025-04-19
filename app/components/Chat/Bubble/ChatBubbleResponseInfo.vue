11b22711be3215b5e953e06073801abd18a12291
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-04-19 00:33:05 -0400

    fix: updating llm provider icons

--- app/components/Chat/Bubble/ChatBubbleResponseInfo.vue
+++ app/components/Chat/Bubble/ChatBubbleResponseInfo.vue
@@ -5,18 +5,6 @@ const props = defineProps<{
     provider: string;
   };
 }>();
-const openAiIcon = resolveComponent("OpenAiIcon");
-const ollamaIcon = resolveComponent("OllamaIcon");
-const currentModelIcon = computed(() => {
-  switch (props.model?.provider) {
-    case "openai":
-      return openAiIcon;
-    case "ollama":
-      return ollamaIcon;
-    default:
-      return undefined;
-  }
-});
 const currentModelName = computed(() => {
   return props.model?.name;
 });
@@ -24,7 +12,10 @@ const currentModelName = computed(() => {
 
 <template>
   <div v-if="model" class="flex items-center gap-1">
-    <component :is="currentModelIcon" class="fill-(--main-color) scale-90" />
+    <Icon
+      :name="`simple-icons:${model.provider}`"
+      class="text-(--main-color) scale-90"
+    />
     <div class="text-(--main-color) text-sm">
       {{ currentModelName }}
     </div>
