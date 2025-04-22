cd1c5780d7eb9bb7f8ed2295f6ee480566eda62e
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-04-21 21:53:36 -0400

    fix: global inject of all font families

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,7 +73,17 @@ export default defineNuxtConfig({
     },
   },
   fonts: {
-    families: [{ name: "Poppins", provider: "google", weight: "bold" }],
+    families: [
+      { name: "Fira Code", provider: "google", global: true },
+      { name: "Geist", provider: "google", global: true },
+      { name: "Georgia", provider: "google", global: true },
+      { name: "IBM Plex Mono", provider: "google", global: true },
+      { name: "Inter", provider: "google", global: true },
+      { name: "Montserrat", provider: "google", global: true },
+      { name: "Nunito", provider: "google", global: true },
+      { name: "Poppins", provider: "google", weight: "bold", global: true },
+      { name: "Roboto Mono", provider: "google", global: true },
+    ],
   },
   components: [
     { path: "~/components", pathPrefix: false },
