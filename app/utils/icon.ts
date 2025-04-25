aa13233231c857024907418c019c5bf6b3f00c49
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-04-24 21:11:55 -0400

    fix: removing console log from provider icon

--- app/utils/icon.ts
+++ app/utils/icon.ts
@@ -1,6 +1,5 @@
 export function getModelProviderIcon(provider?: string) {
   if (!provider) return "";
-  console.log("provider", provider);
   const modelIcons: { [key: string]: string } = {
     ollama: "simple-icons:ollama",
     claude: "simple-icons:claude",
