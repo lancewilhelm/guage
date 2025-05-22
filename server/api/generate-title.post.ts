3180c1a0e996d565acf4e991682a8add4da06de8
Lance Wilhelm <lance.wilhelm@gmail.com>
2025-05-21 22:00:57 -0400

    refactor: moving title generation to new consolidated logic

--- server/api/generate-title.post.ts
+++ server/api/generate-title.post.ts
@@ -1,11 +1,6 @@
 import { logger } from "~/utils/logger";
 import { auth } from "~/utils/auth";
 import type { LocalMessage, Model } from "~/utils/db/local";
-import { completionOpenAI } from "../utils/llm/completionOpenAi";
-import { completionOllama } from "../utils/llm/completionOllama";
-import { completionLMStudio } from "../utils/llm/completionLMStudio";
-import { completionGemini } from "../utils/llm/completionGemini";
-import { completionAnthropic } from "../utils/llm/completionAnthropic";
 
 export default defineEventHandler(async (event) => {
   logger.info("POST /api/generate-title");
@@ -39,49 +34,12 @@ export default defineEventHandler(async (event) => {
   const history = [userMessage];
 
   try {
-    let title;
-    switch (model.provider) {
-      case "openai":
-        title = await completionOpenAI({
-          history,
-          model: model.name,
-          systemPrompt: systemPrompt,
-        });
-        break;
-      case "ollama":
-        title = await completionOllama({
-          history,
-          model: model.name,
-          url: model.url,
-          systemPrompt: systemPrompt,
-        });
-        break;
-      case "gemini":
-        title = await completionGemini({
-          history,
-          model: model.name,
-          systemPrompt: systemPrompt,
-        });
-        break;
-      case "anthropic":
-        title = await completionAnthropic({
-          history,
-          model: model.name,
-          systemPrompt: systemPrompt,
-        });
-        break;
-      case "lmstudio":
-        title = await completionLMStudio({
-          history,
-          model: model.name,
-          systemPrompt: systemPrompt,
-        });
-        break;
-      default:
-        logger.error("POST /api/generate-title: Invalid provider");
-        setResponseStatus(event, 400);
-        return { message: "Invalid provider" };
-    }
+    const title = await providers[model.provider]?.complete({
+      history,
+      model: model.name,
+      systemPrompt,
+      url: model.url,
+    });
 
     return title;
   } catch (error) {
